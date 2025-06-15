4d13d4f3467f25086818777c2e7a2a1383140e18
buzco <gr8successyes@gmail.com>
2025-06-15 03:07:25 +0100

    yoo

--- .eleventy.js
+++ .eleventy.js
@@ -2,10 +2,22 @@ module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("styles.css");
   eleventyConfig.addPassthroughCopy("img");
   eleventyConfig.addPassthroughCopy("scripts.js");
+   eleventyConfig.addPassthroughCopy("admin");
+
+  // Products collection
+  eleventyConfig.addCollection("products", function(collection) {
+    return collection.getFilteredByGlob("./src/products/*.md");
+  });
+
+  // Shop collection (if you have another folder, e.g., src/shop/*.md)
+  eleventyConfig.addCollection("shop", function(collection) {
+    return collection.getFilteredByGlob("./src/shop/*.md");
+  });
+
   return {
     dir: {
       input: "src",
-      includes: "../_includes", // This is needed if your _includes is outside src
+      includes: "../_includes",
       output: "_site"
     }
   };
