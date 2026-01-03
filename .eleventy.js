b9438ee753c8753986374f732539c0307d217127
buzco <gr8successyes@gmail.com>
2026-01-03 21:36:47 +0000

    gemini css optimization

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,27 @@
 const { DateTime } = require("luxon");
 const pluginSitemap = require("@quasibit/eleventy-plugin-sitemap");
 
+import { eleventyImageTransformPlugin } from "@11ty/eleventy-img";
+
+export default function (eleventyConfig) {
+	eleventyConfig.addPlugin(eleventyImageTransformPlugin, {
+		// output image formats
+		formats: ["avif", "webp", "jpeg"],
+
+		// output image widths
+		widths: ["auto"],
+
+		// optional, attributes assigned on <img> nodes override these values
+		htmlOptions: {
+			imgAttributes: {
+				loading: "lazy",
+				decoding: "async",
+			},
+			pictureAttributes: {}
+		},
+	});
+};
+
 module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("styles.css");
   eleventyConfig.addPassthroughCopy("img");
